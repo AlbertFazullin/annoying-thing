efef3108955effffc25630626a4a6f579d4eb14c
nadezhdakharchuk <nadezhda.kharchuk@flatstack.com>
2018-09-07 13:35:23 +0300

    add progress bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
-import Button from '@material-ui/core/Button';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+};
 
 class App extends Component {
+  timer = null;
+
+  state = {
+    completed: 0,
+  };
+  
+  componentDidMount() {
+    this.timer = setInterval(this.progress, 500);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    if (completed === 100) {
+      this.setState({ completed: 0 });
+    } else {
+      const diff = Math.random() * 10;
+      this.setState({ completed: Math.min(completed + diff, 100) });
+    }
+  };
+
   render() {
+    const { classes } = this.props;
     return (
-      <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+      <div>
+        <LinearProgress variant="determinate" value={this.state.completed} />
+      </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
